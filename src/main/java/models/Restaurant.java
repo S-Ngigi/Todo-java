d93562d8f1c6980ecc62e06ba7b1002ca6988a0e
SamNgigi <sam.t.ngigi@gmail.com>
2018-11-24 16:15:05 +0300

    Initial set up of the Yelp-ish project. Added Restaurant model and test it. Test successful

---
+++ src/main/java/models/Restaurant.java
@@ -0,0 +1,162 @@
+package models;
+
+import java.util.Objects;
+
+public class Restaurant {
+    private int id;
+    private String name;
+    private String address;
+    private String zipcode;
+    private String phone;
+    private String website;
+    private String email;
+    private String img_url;
+
+    /*  
+    *  Here we are CONSTRUCTOR OVERLOADING. Here is were we
+    * create a first constructor with the base attributes that we
+    *  require.
+    *
+    * Beneath it we then create another constructor that takes in 
+    * additional attributes that some restaurant do not have.
+    *
+    * This allows us to handle restaurants that may not have a 
+    * website, email or image.
+    *
+    * The amount and type of arguments a method including a
+    * constructor takes are called its METHOD SIGNATURE.
+    *
+    */
+    public Restaurant(String name, String address, String zipcode, String phone) {
+        this.name=name;
+        this.address=address;
+        this.zipcode=zipcode;
+        this.phone=phone;
+        this.website = "No website listed";
+        this.email = "No email available";
+        this.img_url = "No image available";
+    }
+
+    public Restaurant(
+        String name, String address, String zipcode,
+        String phone, String website, 
+         String email, String img_url) {
+
+        this.name = name;
+        this.address = address;
+        this.zipcode = zipcode;
+        this.phone = phone;
+        this.website = website;
+        this.email = email;
+        this.img_url= img_url;
+    }
+
+    /* Getters and Setters */
+
+    public int getRestaurantId(){
+        return id;
+    }
+
+    public void setRestaurantId(int restaurant_id){
+        this.id = restaurant_id;
+    }
+
+    public String getRestaurantName(){
+        return name;
+    }
+
+    public void setRestaurantName(String restaurant_name){
+        this.name = restaurant_name;
+    }
+
+    public String getRestaurantAddress(){
+        return this.address;
+    }
+
+    public void setRestaurantAddress(String restaurant_address){
+        this.address = restaurant_address;
+    }
+
+    public String getRestaurantZipcode(){
+        return zipcode;
+    }
+
+    public void setRestaurantZipcode(String restaurant_zipcode){
+        this.zipcode = restaurant_zipcode;
+    }
+
+    public String getRestaurantPhone(){
+        return phone;
+    }
+
+    public void setRestaurantPhone(String restaurant_phone){
+        this.phone = restaurant_phone;
+    }
+
+    public String getWebsite(){
+        return website;
+    }
+
+    public void setWebsite(String restaurant_website){
+        this.website = restaurant_website;
+    }
+    
+    public String getRestaurantEmail(){
+        return email;
+    }
+
+    public void setRestaurantEmail(String restaurant_email){
+        this.email = restaurant_email;
+    }
+
+    public String getRestaurantImage(){
+        return img_url;
+    }
+
+    public void setRestaurantImage(String restaurant_img){
+        this.img_url = restaurant_img;
+    }
+
+    /*  
+     * The methods below are to make sure that we can make 
+     * accurate comparisons of objects when storing and
+     * retrieving them from our databases.
+     * 
+     * We use the inbuilt Objects.equals() method by passing in
+     * our an our restaurant_object as Object then we compare a
+     *  current instance with the restaurant.
+     * 
+     * We also check if  our arg is null or our this instance and
+     * our arg belong to the same class.
+     * 
+     * We then type cast our Object as a Restaurant class whereby
+     * we check if our property attribute values match.
+     * 
+     * We finally also check the equality of our Restaurant
+     * instance on a memory-based level to see if they match.
+     *  
+     *  Note the @Override annotation(??) allows us to tweak an
+     * existing stock method to our liking.
+     * */
+
+    @Override
+    public boolean equals(Object restaurant_object){
+        if  (this == restaurant_object) return true;
+        if (restaurant_object == null || getClass() != restaurant_object.getClass()) return false;
+        Restaurant restaurant_instance = (Restaurant) restaurant_object;
+        return id == restaurant_instance.id &&
+                                Objects.equals(name, restaurant_instance.name) &&
+                                Objects.equals(address, restaurant_instance.address) &&
+                                Objects.equals(zipcode, restaurant_instance.zipcode) &&
+                                Objects.equals(phone, restaurant_instance.phone) &&
+                                Objects.equals(website, restaurant_instance.website) &&
+                                Objects.equals(email, restaurant_instance.email) &&
+                                Objects.equals(img_url, restaurant_instance.img_url);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(name, address, zipcode, phone, website, email, id, img_url);
+    }
+
+}
---
+++ src/test/java/models/RestaurantTest.java
@@ -0,0 +1,144 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RestaurantTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    /* Testing getRestaurantName method */
+    @Test
+    public void getCorrectRestaurantName() throws Exception {
+        Restaurant test_restaurant = restaurantSetUp();
+        assertEquals("Gramercy Tavern", test_restaurant.getRestaurantName());
+    }
+
+    /* Testing setRestaurantName method */
+    @Test
+    public void setRestaurantNameTest() throws Exception {
+        Restaurant test_restaurant = restaurantAltSetUp();
+        test_restaurant.setRestaurantName(" Eleven Madison Park" );
+        assertNotEquals("Gramercy Tavern",  test_restaurant.getRestaurantName());
+    }
+
+    /* Testing the getRestaurantAddress method */
+    @Test
+    public void getRestaurantAddressTest() throws Exception {
+        Restaurant test_restaurant = restaurantSetUp();
+        assertEquals("42 E 20th St, New York", test_restaurant.getRestaurantAddress());
+    }
+
+    /* Testing the setRestaurantAddress Method */
+    @Test
+    public void setRestaurantAddressTest() throws Exception{
+        Restaurant test_restaurant = restaurantSetUp();
+        test_restaurant.setRestaurantAddress("11 Madison Ave, New York");
+        assertNotEquals("42 E 20th St, New York", test_restaurant.getRestaurantAddress());
+    }
+
+    /* Testing the getRestaurantZipcode method */
+    @Test
+    public void getRestaurantZipcodeTest() throws Exception{
+        Restaurant test_restaurant = restaurantSetUp();
+        assertEquals("NY 10003",  test_restaurant.getRestaurantZipcode());
+    }
+
+    /* Testing the setRestaurantZipcode method */
+    @Test
+    public void setRestaurantZipcodeTest(){
+        Restaurant test_restaurant = restaurantSetUp();
+        test_restaurant.setRestaurantZipcode(" NY 10010");
+        assertNotEquals("NY 10003", test_restaurant.getRestaurantZipcode());
+    }
+
+    /* Testing getRestaurantPhone */
+    @Test
+    public void getRestaurantPhoneTest() throws Exception{
+        Restaurant test_restaurant = restaurantSetUp();
+        assertEquals("+1 212-477-0777", test_restaurant.getRestaurantPhone());
+    }
+
+    /* Testing setRestaurantPhone  */
+    @Test
+    public void setRestaurantPhoneTest() throws Exception {
+        Restaurant test_restaurant = restaurantSetUp();
+        test_restaurant.setRestaurantPhone("+1 212-889-0905");
+        assertNotEquals("+1 212-477-0777" , test_restaurant.getRestaurantPhone());
+    }
+
+    /* Testing getWebsite  */
+    @Test 
+    public void getWebsiteTest() throws Exception{
+        Restaurant test_restaurant = restaurantSetUp();
+        assertEquals("https://www.gramercytavern.com/", test_restaurant.getWebsite());
+    }
+
+    /* Testing setWebsite method */
+    @Test
+    public void setWebsiteTest() throws Exception {
+        Restaurant test_restaurant = restaurantSetUp();
+        test_restaurant.setWebsite("elevenmadisonpark.com");
+        assertNotEquals("https://www.gramercytavern.com/", test_restaurant.getWebsite());
+    }
+
+    /* Testing getRestaurant  email*/
+    @Test
+    public void getRestaurantEmailTest() throws Exception{
+        Restaurant test_restaurant = restaurantAltSetUp();
+        assertEquals("No email available", test_restaurant.getRestaurantEmail());
+    }
+
+    /* Testing setRestaurantEmail */
+    @Test
+    public void setRestaurantEmailTest() throws Exception{
+        Restaurant test_restaurant  = restaurantAltSetUp();
+        test_restaurant.setRestaurantEmail("info@elevenmadisonpark.com");
+        assertNotEquals("No email availabel", test_restaurant.getRestaurantEmail());
+    }
+
+    @Test
+    public void getRestaurantImageTest() throws Exception{
+        Restaurant test_restaurant = restaurantAltSetUp();
+        assertEquals("No image available", test_restaurant.getRestaurantImage());
+    }
+
+    @Test
+    public void setRestaurantImageTest() throws Exception{
+        Restaurant test_restaurant = restaurantAltSetUp();
+        test_restaurant.setRestaurantImage("https://bit.ly/2KwknSj");
+    }
+
+    public Restaurant restaurantSetUp(){
+        Restaurant dummy_restaurant = new Restaurant(
+            "Gramercy Tavern",
+            "42 E 20th St, New York",
+            "NY 10003",
+            "+1 212-477-0777",
+            "https://www.gramercytavern.com/",
+            "events@gramercytavern.com",
+            "https://bit.ly/2Btwb4O"
+        );
+
+        return dummy_restaurant;
+    }
+
+    public Restaurant restaurantAltSetUp(){
+        Restaurant brief_restaurant = new Restaurant(
+            "Gramercy Tavern",
+            "42 E 20th St, New York",
+            "NY 10003",
+            "+1 212-477-0777");
+        return brief_restaurant;
+    }
+}
