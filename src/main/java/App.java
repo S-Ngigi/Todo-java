702013fa5596dcd9a45dc8de24e7221616770dbd
SamNgigi <sam.t.ngigi@gmail.com>
2018-12-15 16:54:39 +0300

    Modulirazing the views for Restaurant successfull

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,61 +1,20 @@
 import org.apache.log4j.Logger;
 import org.apache.log4j.BasicConfigurator;
 
-import com.google.gson.Gson;
-
-import dao.Sql2oRestaurantDao;
-
-import models.Restaurant;
-
-import org.sql2o.Connection;
-import org.sql2o.Sql2o;
+import views.RestaurantViews;
 
 import static spark.Spark.*;
-
 public class App {
 
     static Logger logger = Logger.getLogger(App.class);
     public static void main(String[] args) {
-
+        
         BasicConfigurator.configure();
 
-        Sql2oRestaurantDao restaurant_dao;
-        Connection connect;
-        Gson gson = new Gson(); 
-        String connection_string = "jdbc:h2:~/yelpish_api.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
-        Sql2o sql2o = new Sql2o(connection_string, "", "");
-        restaurant_dao = new Sql2oRestaurantDao(sql2o);
-        connect = sql2o.open();
+        staticFileLocation("public");
 
-        // staticFileLocation("public");
-
-        // *  Accepting a POST request to the specified path in JSON format
-        post("/restaurants/new", "application/json", (request, response) -> {
-            
-            //  * Make Java Object from JSON using GSON
-            Restaurant restaurant = gson.fromJson(request.body(), Restaurant.class);
-            //  * Add the restaurant object to our db using the restaurant dao
-            restaurant_dao.addRestaurant(restaurant);
-            //  * Successful post request
-            response.status(201);
-            response.type("application/json");
-            //  * Lets display the posted restaurant data.
-            return gson.toJson(restaurant);
-        });
-
-        // * Accepting a GET request to get all restaurants
-        get("/restaurants", "application/json", (request, response) -> {
-            response.type("application/json");
-            //  * Display the all the restaurants
-            return gson.toJson(restaurant_dao.getAllRestaurants());
-        });
-       
-        //  * Accepting a GET request to get a specific restaurant
-        get("/restaurant/:id", "application/json", (request, response) -> {
-            response.type("application/json");
-            int restaurant_id = Integer.parseInt(request.params("id"));
-            response.type("application/json");
-            return gson.toJson(restaurant_dao.getRestaurantById(restaurant_id));
-        });
+        RestaurantViews restaurant_view = new RestaurantViews();
+        
+        restaurant_view.restaurantRoutes();
     }
 }
---
+++ src/main/java/views/RestaurantViews.java
@@ -0,0 +1,60 @@
+package views;
+
+import com.google.gson.Gson;
+
+import dao.Sql2oRestaurantDao;
+
+import models.Restaurant;
+
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import static spark.Spark.*;
+
+public class RestaurantViews {
+
+    public  void restaurantRoutes() {
+
+        Sql2oRestaurantDao restaurant_dao;
+        Connection connect;
+        Gson gson = new Gson();
+        String connection_string = "jdbc:h2:~/yelpish_api.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connection_string, "", "");
+        restaurant_dao = new Sql2oRestaurantDao(sql2o);
+        connect = sql2o.open();
+        System.out.println(connect);
+
+        // * Accepting a POST request to the specified path in JSON format
+        post("/restaurants/new", "application/json", (request, response) -> {
+
+            // * Make Java Object from JSON using GSON
+            Restaurant restaurant = gson.fromJson(request.body(), Restaurant.class);
+            // * Add the restaurant object to our db using the restaurant dao
+            restaurant_dao.addRestaurant(restaurant);
+            // * Successful post request
+            response.status(201);
+            // * Lets display the posted restaurant data.
+            return gson.toJson(restaurant);
+        });
+
+        // * Accepting a GET request to get all restaurants
+        get("/restaurants", "application/json", (request, response) -> {
+            // * Display the all the restaurants
+            return gson.toJson(restaurant_dao.getAllRestaurants());
+        });
+
+        // * Accepting a GET request to get a specific restaurant
+        get("/restaurant/:id", "application/json", (request, response) -> {
+            int restaurant_id = Integer.parseInt(request.params("id"));
+            return gson.toJson(restaurant_dao.getRestaurantById(restaurant_id));
+        });
+
+        /*
+         * FILTERS - Allow us to always execute the code block within to execute after
+         * recieving a request allowing for more DRY code.
+         */
+        after((request, response) -> {
+            response.type("application/json");
+        });
+    }
+}
