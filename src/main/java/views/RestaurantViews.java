590359cf2e18d73ca9ec298550aaf914575d1ea8
SamNgigi <sam.t.ngigi@gmail.com>
2018-12-18 05:35:29 +0300

    Refactored code still working and associating well

--- src/main/java/views/RestaurantViews.java
+++ src/main/java/views/RestaurantViews.java
@@ -6,10 +6,10 @@ import java.util.Map;
 import com.google.gson.Gson;
 
 import dao.Sql2oRestaurantDao;
-import dao.Sql2oFoodTypeDao;
+// import dao.Sql2oFoodTypeDao;
 
 import models.Restaurant;
-import models.FoodType;
+// import models.FoodType;
 
 import exceptions.ApiException;
 
@@ -23,13 +23,13 @@ public class RestaurantViews {
     public  void getRestaurantRoutes() {
 
         Sql2oRestaurantDao restaurant_dao;
-        Sql2oFoodTypeDao foodtype_dao;
+        // Sql2oFoodTypeDao foodtype_dao;
         Connection connect;
         Gson gson = new Gson();
         String connection_string = "jdbc:h2:~/yelpish_api.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
         Sql2o sql2o = new Sql2o(connection_string, "", "");
         restaurant_dao = new Sql2oRestaurantDao(sql2o);
-        foodtype_dao = new Sql2oFoodTypeDao(sql2o);
+        // foodtype_dao = new Sql2oFoodTypeDao(sql2o);
         connect = sql2o.open();
         System.out.println(connect);
 
@@ -68,69 +68,6 @@ public class RestaurantViews {
            else {
                 return gson.toJson(found_restaurant);
            }
-        });
-
-        // * Restaurant TO FoodType MANY TO MANY  
-        post("/restaurant/:restaurant_id/foodtype/:foodtype_id", "application/json", (request, response) -> {
-            int restaurant_id = Integer.parseInt(request.params("restaurant_id"));
-            int foodtype_id = Integer.parseInt(request.params("foodtype_id"));
-
-            Restaurant restaurant = restaurant_dao.getRestaurantById(restaurant_id);
-            FoodType foodtype = foodtype_dao.getFoodTypeById(foodtype_id);
-            
-            System.out.println("RESTAURANT FOUND: " + restaurant);
-            System.out.println("FOODTYPE FOUND: " + foodtype);
-
-            if(restaurant != null && foodtype != null) {
-                restaurant_dao.addRestaurantToFoodType(restaurant, foodtype);
-                response.status(201);
-                return gson.toJson(
-                    String.format(
-                        "'%s' and '%s' have been associated",
-                        restaurant.getName(), foodtype.getName()
-                    )
-                );
-            }
-            else {
-                throw new ApiException(404, "Restaurant or FoodType does not exist");
-            }
-        });
-
-        // * GET all FOODTYPES served by a RESTAURANT
-        get("/restaurant/:restaurant_id/foodtypes", "application/json", (request, response) -> {
-            int restaurant_id = Integer.parseInt(request.params("restaurant_id"));
-            Restaurant found_restaurant = restaurant_dao.getRestaurantById(restaurant_id);
-            if(found_restaurant == null) {
-                throw new ApiException(404, String.format(
-                    "Restaurant with the id: %s does not exist",
-                    request.params("restaurant_id")
-                ));
-            }
-            else if (restaurant_dao.getAllFoodTypesByRestaurantId(restaurant_id).size() == 0 ) {
-                return "{\"message\": \"I'm sorry no foodtypes are listed yet for this restaurant.\"}";
-            }
-            else {
-                return gson.toJson(restaurant_dao.getAllFoodTypesByRestaurantId(restaurant_id));
-            }
-        });
-
-        // * GET all RESTAURANTS that serve a particular FOODTYPE
-        get("/foodtype/:foodtype_id/restaurants", "application/json", (request, response) -> {
-            int foodtype_id = Integer.parseInt(request.params("foodtype_id"));
-            FoodType foodtype_result = foodtype_dao.getFoodTypeById(foodtype_id);
-
-            if(foodtype_result == null) {
-                throw new ApiException(404, String.format(
-                    "FoodType with the id : %s does not exist",
-                    request.params("foodtype_id")
-                ));
-            }
-            else if(foodtype_dao.getAllRestaurantsByFoodTypeId(foodtype_id).size() == 0) {
-                return "{\"message\" : \"I'm sorry, but there are no restaurants serving that foodtype yet.\"}";
-            }
-            else {
-                return gson.toJson(foodtype_dao.getAllRestaurantsByFoodTypeId(foodtype_id));
-            }
         });
 
         /*
