349952d4a6aba0f6486bba52e50b5b4b7a41b122
SamNgigi <sam.t.ngigi@gmail.com>
2018-12-18 05:54:21 +0300

    Don't know exactly what was wrong but seem to be Review seems to be getting restaurant by id just fine now

--- src/main/java/views/ReviewView.java
+++ src/main/java/views/ReviewView.java
@@ -5,11 +5,11 @@ import java.util.Map;
 
 import com.google.gson.Gson;
 
-import dao.Sql2oRestaurantDao;
 import dao.Sql2oReviewDao;
+import dao.Sql2oRestaurantDao;
 
-import models.Restaurant;
 import models.Review;
+import models.Restaurant;
 
 import exceptions.ApiException;
 
@@ -19,66 +19,64 @@ import org.sql2o.Sql2o;
 import static spark.Spark.*;
 
 public class ReviewView {
-    
-    public void getReviewRoutes(){
-        Sql2oRestaurantDao restaurant_dao;
+
+    public void getReviewRoutes() {
+
         Sql2oReviewDao review_dao;
+        Sql2oRestaurantDao restaurant_dao;
         Connection connect;
         Gson gson = new Gson();
-        String connection_string = "jdbc:h2:~/yeplish_api.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        String connection_string = "jdbc:h2:~/yelpish_api.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
         Sql2o sql2o = new Sql2o(connection_string, "", "");
         restaurant_dao = new Sql2oRestaurantDao(sql2o);
         review_dao = new Sql2oReviewDao(sql2o);
         connect = sql2o.open();
         System.out.println(connect);
-        
 
-        // *POST a new REVIEW to specific RESTAURANT
+        // * POST a new REVIEW  for a specific RESTAURANT. ONE TO MANY.
         post("/restaurant/:restaurant_id/review/new", "application/json", (request, response) -> {
-            // * restaurant_id for restaurant we want to review.
             int restaurant_id = Integer.parseInt(request.params("restaurant_id"));
-            // * our review for the above restaurant
-            Review review = gson.fromJson(request.body(), Review.class);
-            //  * Setting the restaurant_id property of our review object. One to Many relationship implementation here.
-            review.setRestaurant_id(restaurant_id);
-            // *Adding our review to the db.
-            review_dao.addReview(review);
-            // * returning the review object that we have just posted as json
-            return gson.toJson(review);
+            Restaurant restaurant_result = restaurant_dao.getRestaurantById(restaurant_id);
+            System.out.println("RESTAURANT RESTULT: " + restaurant_result);
+            if(restaurant_result == null){
+                throw new ApiException(404, String.format(
+                    "Restaurant %s does not exist",
+                    request.params("restaurant_id")
+                ));
+            }
+            else {
+                //  * Extracting the review from the request
+                Review review = gson.fromJson(request.body(), Review.class);
+                review.setRestaurant_id(restaurant_id);
+                review_dao.addReview(review);
+                response.status(201);
+                return gson.toJson(review);
+            }
         });
 
-        // * GET all REVIEWS
-        get("/reviews", "application/json", (request, response) -> {
-            return gson.toJson(review_dao.getAllReviews());
-        });
+        /*
+         * FILTERS - Allow us to always execute the code block within to execute after
+         * recieving a request allowing for more DRY code.
+         */
 
-        // * GET all the REVIEWS for a RESTAURANT.
-        get("restaurant/:restaurant_id/reviews", "application/json", (request, response) -> {
-            int restaurant_id = Integer.parseInt(request.params("restaurant_id"));
-            Restaurant found_restaurant = restaurant_dao.getRestaurantById(restaurant_id);
-
-            System.out.println("CAN YOU SEE ME: "+restaurant_id);
-            System.out.println("CAN YOU SEE ME: "+ found_restaurant);
-            return gson.toJson(review_dao.getAllReviewsForRestaurant(restaurant_id));
-            
-        });
-
-        // * FILTERS
         exception(ApiException.class, (exception, request, response) -> {
+            // * We type cast our exception to become our ApiException
             ApiException error = (ApiException) exception;
-            Map <String, Object> gsonJsonMap = new HashMap<>();
+            // * Instantiate a gsonJsonMap hashmap to store our message and status
+            Map<String, Object> gsonJsonMap = new HashMap<>();
+            // * Adding the status to our gsonJsonMap hashmap
             gsonJsonMap.put("status", error.getStatusCode());
+            // * Add the error_message to our gsonJsonMap hashMap
             gsonJsonMap.put("error_message", error.getMessage());
             response.type("application/json");
+            // * Passing the error status code
             response.status(error.getStatusCode());
+            // * Passing our gsonJsonMap as part of our request when exception is thrown.
             response.body(gson.toJson(gsonJsonMap));
         });
-        
 
         after((request, response) -> {
             response.type("application/json");
         });
-
     }
-
 }
