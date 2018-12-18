75f6e4c69a586bb633f4089bf0422a184e5889db
SamNgigi <sam.t.ngigi@gmail.com>
2018-12-18 05:27:46 +0300

    Many to many routes between restaurant and foodtype working. Now to refactor to see if it will work when code is in FoodTypeView module

--- src/main/java/views/ReviewView.java
+++ src/main/java/views/ReviewView.java
@@ -5,11 +5,11 @@ import java.util.Map;
 
 import com.google.gson.Gson;
 
-import dao.Sql2oReviewDao;
 import dao.Sql2oRestaurantDao;
+import dao.Sql2oReviewDao;
 
-import models.Review;
 import models.Restaurant;
+import models.Review;
 
 import exceptions.ApiException;
 
@@ -21,71 +21,46 @@ import static spark.Spark.*;
 public class ReviewView {
     
     public void getReviewRoutes(){
-        Sql2oReviewDao review_dao;
         Sql2oRestaurantDao restaurant_dao;
+        Sql2oReviewDao review_dao;
         Connection connect;
-        
+        Gson gson = new Gson();
         String connection_string = "jdbc:h2:~/yeplish_api.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
         Sql2o sql2o = new Sql2o(connection_string, "", "");
-        Gson gson = new Gson();
-        
-        review_dao = new Sql2oReviewDao(sql2o);
         restaurant_dao = new Sql2oRestaurantDao(sql2o);
+        review_dao = new Sql2oReviewDao(sql2o);
         connect = sql2o.open();
         System.out.println(connect);
+        
 
         // *POST a new REVIEW to specific RESTAURANT
         post("/restaurant/:restaurant_id/review/new", "application/json", (request, response) -> {
             // * restaurant_id for restaurant we want to review.
             int restaurant_id = Integer.parseInt(request.params("restaurant_id"));
-            Restaurant found_restaurant = restaurant_dao.getRestaurantById(restaurant_id);
-            if(found_restaurant == null) {
-                throw new ApiException(404, String.format("Restaurant with id: %s does not exists", request.params("restaurant_id")));
-            }
-            else {
-                // * our review for the above restaurant
-                Review review = gson.fromJson(request.body(), Review.class);
-                // * Setting the restaurant_id property of our review object. One to Many
-                // relationship implementation here.
-                review.setRestaurant_id(restaurant_id);
-                // *Adding our review to the db.
-                review_dao.addReview(review);
-                // * returning the review object that we have just posted as json
-                return gson.toJson(review);
-            }
-
-        });
-
-        // * GET all the REVIEWS for a RESTAURANT.
-        get("restaurant/:id/reviews", "application/json", (request, response) -> {
-            int restaurant_id = Integer.parseInt(request.params("id"));
-            return gson.toJson(review_dao.getAllReviewsForRestaurant(restaurant_id));
-            // int restaurant_id = Integer.parseInt(request.params("id"));
-            // Restaurant found_restaurant = restaurant_dao.getRestaurantById(restaurant_id);
-            // int restaurant_id = Integer.parseInt(request.params("id"));
-            // Restaurant found_restaurant = restaurant_dao.getRestaurantById(restaurant_id);
-            // System.out.println("CAN YOU SEE ME: "+found_restaurant);
-            // if(found_restaurant == null){
-            //    throw new ApiException(404, String.format("Restaurant with id: %s does not exists", request.params("id")));
-            // }
-            // else if (review_dao.getAllReviewsForRestaurant(restaurant_id).size() == 0){
-            //     throw new ApiException(404, "No reviews yet for this restaurant");
-            // }
-            // else {
-            //     return gson.toJson(review_dao.getAllReviewsForRestaurant(restaurant_id));
-                
-            // }
-           
+            // * our review for the above restaurant
+            Review review = gson.fromJson(request.body(), Review.class);
+            //  * Setting the restaurant_id property of our review object. One to Many relationship implementation here.
+            review.setRestaurant_id(restaurant_id);
+            // *Adding our review to the db.
+            review_dao.addReview(review);
+            // * returning the review object that we have just posted as json
+            return gson.toJson(review);
         });
 
         // * GET all REVIEWS
-        get("/reviews", "application/json", (request, respons) -> {
-            if(review_dao.getAllReviews().size() == 0) {
-                throw new ApiException(404, "No reviews posted yet");
-            } 
-            else {
-                return gson.toJson(review_dao.getAllReviews());
-            }
+        get("/reviews", "application/json", (request, response) -> {
+            return gson.toJson(review_dao.getAllReviews());
+        });
+
+        // * GET all the REVIEWS for a RESTAURANT.
+        get("restaurant/:restaurant_id/reviews", "application/json", (request, response) -> {
+            int restaurant_id = Integer.parseInt(request.params("restaurant_id"));
+            Restaurant found_restaurant = restaurant_dao.getRestaurantById(restaurant_id);
+
+            System.out.println("CAN YOU SEE ME: "+restaurant_id);
+            System.out.println("CAN YOU SEE ME: "+ found_restaurant);
+            return gson.toJson(review_dao.getAllReviewsForRestaurant(restaurant_id));
+            
         });
 
         // * FILTERS
@@ -98,6 +73,7 @@ public class ReviewView {
             response.status(error.getStatusCode());
             response.body(gson.toJson(gsonJsonMap));
         });
+        
 
         after((request, response) -> {
             response.type("application/json");
